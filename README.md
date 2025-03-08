aec5a717ade0cd2949193642dda8a2684d4400ea
ejmeitz <ejmeitz1@gmail.com>
2025-03-08 16:09:22 -0500

    wrapper builds

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Legate.jl
 Julia Bindings for nv-legate
+
+
+Pre-Requesites:
+- Legate installed into a conda environment
+
+
+Build Legate Wrapper Library:
+1. Activate legate conda environment
+2. `sh ./scripts/build.sh`
--- wrapper/src/wrapper.cpp
+++ wrapper/src/wrapper.cpp
@@ -1,19 +1,21 @@
 #include "legate.h"
+#include "jlcxx/jlcxx.hpp"
+
+#include <type_traits>
+#include <vector>
 
 #include "types.h"
 
-#include <vector>
-
 using namespace legate;
+
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
 
     wrap_type_enums(mod); // legate::Type
 
-    // Need to wrap LogicalArray, LogicalStore
 
-    mod.add_type<LocalTaskID>("LocalTaskID");
-    mod.add_type<GlobalTaskID>("GlobalTaskID");
+    mod.map_type<LocalTaskID>("LocalTaskID");
+    mod.map_type<GlobalTaskID>("GlobalTaskID");
 
     mod.add_type<Shape>("Shape")
         .constructor<std::vector<std::uint64_t>>();
@@ -33,37 +35,37 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
         .method("nullable", &PhysicalArray::nullable)
         .method("dim", &PhysicalArray::dim)
         .method("type", &PhysicalArray::type)
-        .method("data", &PhysicalArray::data)
+        .method("data", &PhysicalArray::data);
 
     mod.add_type<LogicalArray>("LogicalArray")
         .method("dim", &LogicalArray::dim)
         .method("type", &LogicalArray::type)
         .method("shape", &LogicalArray::shape)
         .method("unbound", &LogicalArray::unbound)
-        .method("nullable", &LogicalArray::nullable)
-        .method("data", &LogicalStore::data)
-        .method("get_physical_array", &LogicalStore::get_physical_array)
+        .method("nullable", &LogicalArray::nullable);
 
     mod.add_type<AutoTask>("AutoTask");
     mod.add_type<ManualTask>("ManualTask");
 
     mod.add_type<Runtime>("LegateRuntime")
         .method("create_auto_task", static_cast<AutoTask (Runtime::*)(Library, LocalTaskID)>(&Runtime::create_task))
-        .method("submit_auto_task", static_cast<void (Runtime::*)(AutoTask&&)>(&Runetime::submit))
-        .method("submit_manual_task", static_cast<void (Runtime::*)>(ManualTask&&)(&Runtime::submit))
-        .method("create_unbound_array", static_cast<LogicalArray (Runtime::*)(const Type*, std::uint32_t, bool)>(&Runtime::create_array),
-                 jlcxx::kwarg("dim") = 1, jlcxx::kwrag("nullable") = false)
+        // issues with r-value references. Probably need to wrap a function and std::move the Task object
+        // .method("submit_auto_task", static_cast<void (Runtime::*)(AutoTask&&)>(&Runtime::submit))
+        // .method("submit_manual_task", static_cast<void (Runtime::*)>(ManualTask&&)(&Runtime::submit))
+        .method("create_unbound_array", static_cast<LogicalArray (Runtime::*)(const Type&, std::uint32_t, bool)>(&Runtime::create_array),
+                 jlcxx::kwarg("dim") = 1, jlcxx::kwarg("nullable") = false)
         .method("create_array", static_cast<LogicalArray (Runtime::*)(const Shape&, const Type&, bool, bool)>(&Runtime::create_array),
                  jlcxx::kwarg("nullable") = false, jlcxx::kwarg("optimize_scalar") = false)
         .method("create_unbound_store", static_cast<LogicalStore (Runtime::*)(const Type&, std::uint32_t)>(&Runtime::create_store), 
                  jlcxx::kwarg("dim") = 1)
         .method("create_store", static_cast<LogicalStore (Runtime::*)(const Shape&, const Type&, bool)>(&Runtime::create_store),
                  jlcxx::kwarg("optimize_scalar") = false)
-        .method("store_from_scalar", static_cast<LogicalStore (Runtime::*)(const Scalar&, const Shape&)(&Runtime::create_store),
-                 jlcxx::kwarg("shape") = Shape{1})
-        .method("start", &Runtime::start)
-        .method("has_started", &Runtime::start)
-        .method("finish", &Runtime::finish)
-        .method("has_finished", &Runtime::has_finished);
+        .method("store_from_scalar", static_cast<LogicalStore (Runtime::*)(const Scalar&, const Shape&)>(&Runtime::create_store),
+                 jlcxx::kwarg("shape") = Shape{1});
+
+    mod.method("start", static_cast<void (*)()>(&legate::start));
+    mod.method("has_started", &legate::has_started);
+    mod.method("finish", &legate::finish);
+    mod.method("has_finished", &legate::has_finished);
 
 }
