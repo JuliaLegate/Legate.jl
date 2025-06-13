9c8736b1889f5a781a5110031ac534ff894c5d1c
krasow <krasow@u.northwestern.edu>
2025-06-13 00:51:11 -0500

    support other create array functions

--- wrapper/src/wrapper.cpp
+++ wrapper/src/wrapper.cpp
@@ -35,6 +35,26 @@
 
 using namespace legate;
 
+legate::Type type_from_code(legate::Type::Code type_id) {
+  switch (type_id) {
+    case legate::Type::Code::BOOL:       return legate::bool_();
+    case legate::Type::Code::INT8:       return legate::int8();
+    case legate::Type::Code::INT16:      return legate::int16();
+    case legate::Type::Code::INT32:      return legate::int32();
+    case legate::Type::Code::INT64:      return legate::int64();
+    case legate::Type::Code::UINT8:      return legate::uint8();
+    case legate::Type::Code::UINT16:     return legate::uint16();
+    case legate::Type::Code::UINT32:     return legate::uint32();
+    case legate::Type::Code::UINT64:     return legate::uint64();
+    case legate::Type::Code::FLOAT16:    return legate::float16();
+    case legate::Type::Code::FLOAT32:    return legate::float32();
+    case legate::Type::Code::FLOAT64:    return legate::float64();
+    case legate::Type::Code::COMPLEX64:  return legate::complex64();
+    case legate::Type::Code::COMPLEX128: return legate::complex128();
+    default:
+      throw std::invalid_argument("Unsupported legate::Type::Code enum value.");
+  }
+}
 
 struct WrapDefault {
     template <typename TypeWrapperT>
@@ -152,47 +172,51 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
       .method("create_auto_task", [](Runtime* rt, Library lib, LocalTaskID id) { return rt->create_task(lib, id); })
       .method("submit_auto_task", [](Runtime* rt, AutoTask& task) { return rt->submit(std::move(task));})
       .method("submit_manual_task", [](Runtime* rt, ManualTask& task) {return rt->submit(std::move(task));});
-      
+          
     mod.method("get_runtime", [] { return Runtime::get_runtime(); });
+    mod.method("create_unbound_array",
+      [](const Type& ty, std::uint32_t dim = 1, bool nullable = false) {
+        // Type ty = type_from_code(Type::Code(type_id));
+        return Runtime::get_runtime()->create_array(ty, dim, nullable);
+      } //,
+      // jlcxx::kwarg("dim") = 1,
+      // jlcxx::kwarg("nullable") = false
+    );
 
-    // mod.method("create_unbound_array",
-    //       [](const Type& ty, std::uint32_t dim = 1, bool nullable = false) {
-    //         return Runtime::get_runtime()->create_array(ty, dim, nullable);
-    //       },
-    //       jlcxx::kwarg("dim") = 1,
-    //       jlcxx::kwarg("nullable") = false
-    // );
-    // mod.method("create_array",
-    //   [](const Shape& shape, const Type& ty, bool nullable = false, bool optimize_scalar = false) {
-    //     return Runtime::get_runtime()->create_array(shape, ty, nullable, optimize_scalar);
-    //   },
-    //   jlcxx::kwarg("nullable") = false,
-    //   jlcxx::kwarg("optimize_scalar") = false
-    // );
+    mod.method("create_array",
+      [](const Shape& shape, const Type& ty, bool nullable = false, bool optimize_scalar = false) {
+        // Type ty = type_from_code(Type::Code(type_id));
+        return Runtime::get_runtime()->create_array(shape, ty, nullable, optimize_scalar);
+      } //,
+      // jlcxx::kwarg("nullable") = false,
+      // jlcxx::kwarg("optimize_scalar") = false
+    );
 
-    // // create_unbound_store with Type, uint32_t
-    // mod.method("create_unbound_store",
-    //   [](const Type& ty, std::uint32_t dim = 1) {
-    //     return Runtime::get_runtime()->create_store(ty, dim);
-    //   },
-    //   jlcxx::kwarg("dim") = 1
-    // );
+    // create_unbound_store with Type, uint32_t
+    mod.method("create_unbound_store",
+      [](const Type& ty, std::uint32_t dim = 1) {
+        // Type ty = type_from_code(Type::Code(type_id));
+        return Runtime::get_runtime()->create_store(ty, dim);
+      } //,
+      // jlcxx::kwarg("dim") = 1
+    );
 
     // // create_store with Shape, Type, bool
-    // mod.method("create_store",
-    //   [](const Shape& shape, const Type& ty, bool optimize_scalar = false) {
-    //     return Runtime::get_runtime()->create_store(shape, ty, optimize_scalar);
-    //   },
-    //   jlcxx::kwarg("optimize_scalar") = false
-    // );
+    mod.method("create_store",
+      [](const Shape& shape, const Type& ty, bool optimize_scalar = false) {
+        // Type ty = type_from_code(Type::Code(type_id));
+        return Runtime::get_runtime()->create_store(shape, ty, optimize_scalar);
+      } //,
+      // jlcxx::kwarg("optimize_scalar") = false
+    );
 
-    // // store_from_scalar (Shape as kwarg)
-    // mod.method("store_from_scalar",
-    //   [](const Scalar& scalar, const Shape& shape = Shape{1}) {
-    //     return Runtime::get_runtime()->create_store(scalar, shape);
-    //   },
-    //   jlcxx::kwarg("shape") = Shape{1}
-    // );
+    // store_from_scalar (Shape as kwarg)
+    mod.method("store_from_scalar",
+      [](const Scalar& scalar, const Shape& shape = Shape{1}) {
+        return Runtime::get_runtime()->create_store(scalar, shape);
+      } //,
+      // jlcxx::kwarg("shape") = Shape{1}
+    );
 
     // intialization & cleanup
     // TODO catch the (Auto)ConfigurationError and make the Julia error nicer
