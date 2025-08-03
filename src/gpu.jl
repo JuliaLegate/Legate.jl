bc641d063299bcf6cd93930f2f46bdbcea15776d
krasow <krasow@u.northwestern.edu>
2025-08-03 12:55:17 -0500

    global scope fix

--- src/gpu.jl
+++ src/gpu.jl
@@ -1,14 +1,7 @@
+using CUDA
+using CUDA_Driver_jll
+
 module GPUSetup
-
-# Ensure CUDA and CUDA_Driver_jll are loaded into Main (global)
-if !isdefined(Main, :CUDA)
-    Core.eval(Main, :(using CUDA))
-end
-
-if !isdefined(Main, :CUDA_Driver_jll)
-    Core.eval(Main, :(using CUDA_Driver_jll))
-end
-
 # Load libcuda globally
 const cuda_driver_dir = joinpath(CUDA_Driver_jll.artifact_dir, "lib")
 const libcuda_path = joinpath(cuda_driver_dir, "libcuda.so")
