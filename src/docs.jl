fc7824628acef5dc4a09e027cfde9752f5760313
krasow <krasow@u.northwestern.edu>
2025-08-02 23:04:07 -0500

    remove docs.jl

--- src/docs.jl
+++
@@ -1,9 +0,0 @@
- # add these dummy functions for documentation
-const code_type_map = Dict{Int, DataType}(
-        1 => Float32,
-        2 => Float64,
-        3 => Int64,
-    )
-function to_legate_type(::Type{T}) where T
-    return T
-end
