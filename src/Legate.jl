91d9909dd52f25edf452a79c8a38a58a99a12876
krasow <krasow@u.northwestern.edu>
2025-08-03 23:31:42 -0500

    attempt to fix cutensor problem

--- src/Legate.jl
+++ src/Legate.jl
@@ -47,6 +47,8 @@ deps_path = joinpath(@__DIR__, "../", "deps", "deps.jl")
 
 if isfile(deps_path)
     include(deps_path)
+    push!(Base.DL_LOAD_PATH, CUDA_RUNTIME_LIB)
+    push!(Base.DL_LOAD_PATH, CUDA_DRIVER_LIB)
 else
     include("gpu.jl")
 
