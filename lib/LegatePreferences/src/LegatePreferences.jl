04d0793e139cf7a45e5a7a86a7a59add4e3da4e7
krasow <krasow@u.northwestern.edu>
2025-12-02 15:54:52 -0600

    modular system

--- lib/LegatePreferences/src/LegatePreferences.jl
+++ lib/LegatePreferences/src/LegatePreferences.jl
@@ -18,163 +18,13 @@
 =#
 
 module LegatePreferences
-
 using Preferences
-
 using Libdl
 
-const PREFS_CHANGED = Ref(false)
-const DEPS_LOADED = Ref(false)
+include("PreferenceBackend.jl")
+using .PrefBackend
 
-abstract type Mode end
-struct JLL <: Mode end # default
-struct Developer <: Mode end # will compile wrappers from src
-struct Conda <: Mode end # not well tested, allows conda env install
-
-function to_mode(m::String)
-    m_lower = lowercase(m)
-    if m_lower == "jll"
-        return JLL()
-    elseif m_lower == "developer"
-        return Developer()
-    elseif m_lower == "conda"
-        return Conda()
-    else
-        error("Unknown mode: $(m). Must be one of 'jll', 'developer', or 'conda'.")
-    end
-end
-
-const MODE_JLL = "jll"
-const MODE_DEVELOPER = "developer"
-const MODE_CONDA = "conda"
-
-const MODE = @load_preference("mode", MODE_JLL)
-
-# used for developer mode
-const wrapper_branch = @load_preference("wrapper_branch")
-const use_legate_jll = @load_preference("use_legate_jll")
-const legate_path = @load_preference("legate_path")
-
-# used for conda mode
-const conda_env = @load_preference("conda_env")
-
-# default developer options
-const DEVEL_DEFAULT_JLL_WRAP_CONFIG = false
-const DEVEL_DEFAULT_WRAPPER_BRANCH = "main"
-const DEVEL_DEFAULT_JLL_CONFIG = true
-const DEVEL_DEFAULT_LEGATE_PATH = nothing
-
-# from MPIPreferences.jl
-"""
-    LegatePreferences.check_unchanged()
-
-Throws an error if the preferences have been modified in the current Julia
-session, or if they are modified after this function is called.
-
-This is should be called from the `__init__()` function of any package which
-relies on the values of LegatePreferences.
-"""
-function check_unchanged()
-    if PREFS_CHANGED[]
-        error("LegatePreferences have changed, you will need to restart Julia for the changes to take effect")
-    end
-    DEPS_LOADED[] = true
-    return nothing
-end
-
-
-"""
-    LegatePreferences.use_conda(conda_env::String; export_prefs = false, force = true)
-
-Tells Legate.jl to use existing conda install. We make no gurantees of compiler compatability at this time.
-
-Expects `conda_env` to be the absolute path to the root of the environment.
-For example, `/home/julialegate/.conda/envs/cunumeric-gpu`
-"""
-function use_conda(conda_env::String; export_prefs = false, force = true)
-    set_preferences!(LegatePreferences,
-        "conda_env" =>  conda_env,
-        "mode" => MODE_CONDA,
-        export_prefs = export_prefs,
-        force = force
-    )
-
-    if conda_env == LegatePreferences.conda_env && LegatePreferences.mode == MODE_CONDA
-        @info "LegatePreferences found no differences."
-    else
-        PREFS_CHANGED[] = true
-        @info "LegatePreferences set to use local conda env at:" conda_env
-
-        if DEPS_LOADED[]
-            error("You will need to restart Julia for the changes to take effect. You may need Pkg.build() for Legion patch.")
-        end
-    end
-end
-
-"""
-    LegatePreferences.use_jll_binary(; export_prefs = false, force = true)
-
-Tells Legate.jl to use JLLs. This is the default option. 
-"""
-function use_jll_binary(; export_prefs = false, force = true)
-    set_preferences!(LegatePreferences,
-        "mode" => MODE_JLL,
-        export_prefs = export_prefs,
-        force = force
-    )
-
-    if LegatePreferences.mode == MODE_JLL
-        @info "LegatePreferences found no differences. Using JLLs."
-    else
-        PREFS_CHANGED[] = true
-        @info "LegatePreferences set to use JLLs."
-
-        if DEPS_LOADED[]
-            error("You will need to restart Julia for the changes to take effect. JLLs do not require building.")
-        end
-    end
-end
-
-"""
-    LegatePreferences.use_developer_mode(; wrapper_branch="main", use_legate_jll=true, legate_path=nothing, export_prefs = false, force = true)
-
-Tells Legate.jl to enable developer mode. This will clone legate_jl_wrapper into Legate.jl/deps. 
-
-To specify a legate_jl_wrapper branch: ```wrapper_branch="some-branch"```
-To disable using legate_jll: ```use_legate_jll=false``` 
-If you disable legate_jll, then you need to set a path to Legate with ```legate_path="/path/to/legate"```
-"""
-function use_developer_mode(; wrapper_branch=DEVEL_DEFAULT_WRAPPER_BRANCH, use_legate_jll=DEVEL_DEFAULT_JLL_CONFIG, legate_path=DEVEL_DEFAULT_LEGATE_PATH, export_prefs = false, force = true)
-    if (use_legate_jll == false)
-        if (isnothing(legate_path)) 
-            error("You must set a legate_path if you are disabling use_legate_jll")
-        end
-    end
-
-    set_preferences!(LegatePreferences,
-        "mode" => MODE_DEVELOPER,
-        "wrapper_branch" => wrapper_branch,
-        "use_legate_jll" => use_legate_jll,
-        "legate_path" => legate_path,
-        export_prefs = export_prefs,
-        force = force
-    )
-    same_branch = wrapper_branch == LegatePreferences.wrapper_branch
-    same_jll_conf = use_legate_jll == LegatePreferences.use_legate_jll
-    same_legate_path = legate_path == LegatePreferences.legate_path
-    same_mode = LegatePreferences.MODE == MODE_DEVELOPER
-    
-    if same_branch && same_jll_conf && same_legate_path && same_mode
-        @info "LegatePreferences found no differences. Using Developer mode."
-    else
-        PREFS_CHANGED[] = true
-        @info "LegatePreferences set to use developer mode.."
-
-        if DEPS_LOADED[]
-            error("You will need to restart Julia for the changes to take effect. You need to call Pkg.build()")
-        end
-    end
-end
+@make_preferences("legate_")
 
 # Mimics: https://github.com/JuliaGPU/CUDA.jl/blob/e0ec269e2a84df29e17f8588569e37e79e049606/lib/cudadrv/version.jl#L41
 function fake_cuda_local_preferences(version::Union{Nothing,VersionNumber}=nothing;
@@ -234,11 +84,10 @@ function has_cuda_gpu()::Bool
 end
 
 function __init__()
-
     # We do not support CUDA 13 yet so force it to install CUDA 12.8
     if MODE == MODE_JLL
         if has_cuda_gpu()
-            # @info "Deteced CUDA GPU"
+            # @info "Detected CUDA GPU"
             fake_cuda_local_preferences(v"12.8"; local_toolkit=false)
         else
             @warn "Detected no CUDA GPU will download CPU only JLL."
---
+++ lib/LegatePreferences/src/PreferenceBackend.jl
@@ -0,0 +1,161 @@
+module PrefBackend
+
+export @make_preferences
+
+"""
+    @make_preferences(; prefix, default_mode="jll",
+                        default_use_jll=true,
+                        default_path=nothing)
+Creates a namespaced set of preferences for Legate.jl or cuNumeric.jl.  
+The generated preferences and functions will be prefixed with the provided
+`prefix` string.    
+# Arguments
+- `prefix::String`: The prefix to use for all preference keys and generated functions.
+- `default_mode::String`: The default mode to use. One of "jll", "developer", or "conda". Default is "jll".
+- `default_use_jll::Bool`: The default value for whether to use JLLs in developer mode. Default is `true`.
+- `default_path::Union{Nothing,String}`: The default path to Legate or cuNumeric in developer mode. Default is `nothing`.
+""" 
+macro make_preferences(prefix, default_mode="jll",
+                        default_use_jll=true,
+                        default_path=nothing)
+
+esc(quote
+    abstract type Mode end
+    struct JLL <: Mode end # default
+    struct Developer <: Mode end # will compile wrappers from src
+    struct Conda <: Mode end # not well tested, allows conda env install
+
+    function to_mode(m::String)
+        m_lower = lowercase(m)
+        if m_lower == "jll"
+            return JLL()
+        elseif m_lower == "developer"
+            return Developer()
+        elseif m_lower == "conda"
+            return Conda()
+        else
+            error("Unknown mode: $(m). Must be one of 'jll', 'developer', or 'conda'.")
+        end
+    end
+
+    const MODE_JLL = "jll"
+    const MODE_DEVELOPER = "developer"
+    const MODE_CONDA = "conda"
+
+    const _PREFS_CHANGED = Ref(false)
+    const _DEPS_LOADED = Ref(false)
+
+    # load preferences with namespaced keys
+    const MODE = @load_preference($prefix * "mode", $default_mode)
+
+    const use_jll = @load_preference($prefix * "use_jll",
+                                            $default_use_jll)
+    const path = @load_preference($prefix * "path", $default_path)
+    const conda_env = @load_preference($prefix * "conda_env")
+
+    function _set(pairs::Pair...; export_prefs=false, force=true)
+        prefixed_pairs = Pair[]
+        for (k, v) in pairs
+            push!(prefixed_pairs, $prefix * k => v)
+        end
+
+        set_preferences!(@__MODULE__, prefixed_pairs...; export_prefs, force)
+    end
+
+    """
+        LegatePreferences.check_unchanged()
+
+    Throws an error if the preferences have been modified in the current Julia
+    session, or if they are modified after this function is called.
+
+    This is should be called from the `__init__()` function of any package which
+    relies on the values of LegatePreferences.
+    """
+    function check_unchanged()
+        if _PREFS_CHANGED[]
+            error("Preferences changed; restart Julia")
+        end
+        _DEPS_LOADED[] = true
+        nothing
+    end
+
+
+    """
+        LegatePreferences.use_jll_binary(; export_prefs = false, force = true)
+
+    Tells Legate.jl | cuNumeric.jl to use JLLs. This is the default option. 
+    """
+
+    function use_jll_binary(; export_prefs=false, force=true)
+        if MODE == MODE_JLL
+            @info "Already using JLL mode"
+        else
+            _PREFS_CHANGED[] = true
+            @info "Switched to JLL mode"
+            _set("mode" => MODE_JLL; export_prefs, force)
+            if _DEPS_LOADED[]
+                error("JLL mode: Restart Julia for changes to take effect.")
+            end
+        end
+    end
+
+    """
+        LegatePreferences.use_conda(conda_env::String; export_prefs = false, force = true)
+
+    Tells Legate.jl | cuNumeric.jl to use existing conda install. We make no gurantees of compiler compatability at this time.
+
+    Expects `conda_env` to be the absolute path to the root of the environment.
+    For example, `/home/julialegate/.conda/envs/cunumeric-gpu`
+    """
+    function use_conda(env; export_prefs=false, force=true)
+        if MODE == MODE_CONDA
+            @info "Already using Conda mode"
+        else
+            _PREFS_CHANGED[] = true
+            @info "Conda mode enabled with env=$env"
+            _set("mode" => MODE_CONDA, "conda_env" => env; export_prefs, force)
+            if _DEPS_LOADED[]
+                error("Conda mode: Restart Julia for changes to take effect. You will need Pkg.build().")
+            end
+        end
+    end
+
+    """
+    LegatePreferences.use_developer_mode(; use_jll=true, path=nothing, export_prefs = false, force = true)
+
+    Tells Legate.jl | cuNumeric.jl to enable developer mode. Developer mode allows you to build from source.
+
+    To disable using legate_jll or cupynumeric_jll: ```use_jll=false``` 
+    If you disable legate_jll or cupynumeric_jll, then you need to set a path to Legate|cuPyNumeric with ```path="/path/to/Legate|cuPyNumeric"```
+    """
+    function use_developer_mode(;use_jll=$default_use_jll,
+                                 path=$default_path,
+                                 export_prefs=false, force=true)
+
+        if !use_jll && isnothing(path)
+            error("Must provide path when not using JLL")
+        end
+
+        if MODE == MODE_DEVELOPER && use_jll == use_jll && path == path
+            @info "Already using Developer mode with the same settings"
+            return
+        end
+
+        _PREFS_CHANGED[] = true
+
+        _set("mode" => MODE_DEVELOPER,
+             "use_jll" => use_jll,
+             "path" => path;
+             export_prefs, force)
+
+        @info "Developer mode enabled"
+        
+        if _DEPS_LOADED[]
+            error("Developer Mode: Restart Julia for changes to take effect. You will need to run Pkg.build().")
+        end
+    end
+end)
+
+end
+
+end # module
