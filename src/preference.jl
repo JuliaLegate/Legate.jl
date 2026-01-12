7c07591e446258bfc7196cb85dce575257eb24c3
ejmeitz <emeitz@andrew.cmu.edu>
2026-01-12 15:53:13 -0600

    oops

--- src/preference.jl
+++ src/preference.jl
@@ -160,7 +160,7 @@ const DEPS_MAP = Dict(
     "CUDA_DRIVER" => "libcuda",
     "CUDA_RUNTIME" => "libcudart"
 )
-function find_dependency_paths(mode::LegatePreferences.JLL)
+function find_dependency_paths(::Type{LegatePreferences.JLL})
     results = Dict{String, String}()
 
     paths_to_search = copy(legate_jll.LIBPATH_list)
