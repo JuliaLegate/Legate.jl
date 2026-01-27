dbcc0c711ad0b07d17f2c6ac4be91edce208dd77
Documenter.jl <documenter@juliadocs.github.io>
2026-01-27 20:02:21 +0000

    build based on 3118b1f

--- previews/PR62/CppAPI/structufi_1_1TaskRequestData.js
+++ previews/PR62/CppAPI/structufi_1_1TaskRequestData.js
@@ -2,6 +2,8 @@ var structufi_1_1TaskRequestData =
 [
     [ "inputs_ptr", "structufi_1_1TaskRequestData.html#a87f6f84930980a2995c3f8818c5aa3de", null ],
     [ "n", "structufi_1_1TaskRequestData.html#ab5482fe1b990406c4e6bc4d09d0ba847", null ],
+    [ "num_inputs", "structufi_1_1TaskRequestData.html#ab8398aec78c255575481e45d2cc74051", null ],
+    [ "num_outputs", "structufi_1_1TaskRequestData.html#a238282d5c882e5ecf9847cb85abed0cd", null ],
     [ "outputs_ptr", "structufi_1_1TaskRequestData.html#a4a949082de7ab749ae73d6697e67a94a", null ],
     [ "task_id", "structufi_1_1TaskRequestData.html#a002ea68340416017165c2e19cb146fe5", null ]
 ];
