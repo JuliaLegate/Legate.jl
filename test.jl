85d792a6a0a75851517e3776008e043067c5fab6
krasow <krasow@u.northwestern.edu>
2026-01-28 11:26:51 -0600

    save this state. only works with debug enabled

--- test.jl
+++ test.jl
@@ -43,70 +43,68 @@ function test_driver()
     rt = Legate.get_runtime()
     lib = Legate.create_library("test")
 
-    # Wrap Legate operations in @async blocks
-    # This keeps the main thread's event loop active!
-    return @async begin
-        # 1. Init Task (3 args)
-        a = Legate.create_array([10, 10], Float32)
-        b = Legate.create_array([10, 10], Float32)
-        c = Legate.create_array([10, 10], Float32)
-        d = Legate.create_array([10, 10], Float32) # Extra array for 4-arg test
+    # 1. Init Task (3 args)
+    a = Legate.create_array([10, 10], Float32)
+    b = Legate.create_array([10, 10], Float32)
+    c = Legate.create_array([10, 10], Float32)
+    d = Legate.create_array([10, 10], Float32) # Extra array for 4-arg test
 
-        task = Legate.create_julia_task(rt, lib, my_init_task)
-        init_output_vars = Vector{Legate.Variable}()
-        push!(init_output_vars, Legate.add_output(task, a))
-        push!(init_output_vars, Legate.add_output(task, b))
-        push!(init_output_vars, Legate.add_output(task, c))
-        Legate.default_alignment(task, Vector{Legate.Variable}(), init_output_vars)
+    task = Legate.create_julia_task(rt, lib, my_init_task)
+    init_output_vars = Vector{Legate.Variable}()
+    push!(init_output_vars, Legate.add_output(task, a))
+    push!(init_output_vars, Legate.add_output(task, b))
+    push!(init_output_vars, Legate.add_output(task, c))
+    Legate.default_alignment(task, Vector{Legate.Variable}(), init_output_vars)
 
-        Legate.submit_task(rt, task)
+    Legate.submit_task(rt, task)
 
-        # 2. Compute Task (3 args)
-        task2 = Legate.create_julia_task(rt, lib, my_task)
-        input_vars = Vector{Legate.Variable}()
-        output_vars = Vector{Legate.Variable}()
-        push!(input_vars, Legate.add_input(task2, a))
-        push!(input_vars, Legate.add_input(task2, b))
-        push!(output_vars, Legate.add_output(task2, c))
-        Legate.default_alignment(task2, input_vars, output_vars)
+    # 2. Compute Task (3 args)
+    task2 = Legate.create_julia_task(rt, lib, my_task)
+    input_vars = Vector{Legate.Variable}()
+    output_vars = Vector{Legate.Variable}()
+    push!(input_vars, Legate.add_input(task2, a))
+    push!(input_vars, Legate.add_input(task2, b))
+    push!(output_vars, Legate.add_output(task2, c))
+    Legate.default_alignment(task2, input_vars, output_vars)
 
-        Legate.submit_task(rt, task2)
+    Legate.submit_task(rt, task2)
 
-        # 3. Arbitrary Arg Task (4 args: 2 in, 2 out)
-        task3 = Legate.create_julia_task(rt, lib, my_4arg_task)
-        in_vars_4 = Vector{Legate.Variable}()
-        out_vars_4 = Vector{Legate.Variable}()
-        # Inputs: a, c
-        push!(in_vars_4, Legate.add_input(task3, a))
-        push!(in_vars_4, Legate.add_input(task3, c))
-        # Outputs: b (reuse), d (new)
-        push!(out_vars_4, Legate.add_output(task3, b))
-        push!(out_vars_4, Legate.add_output(task3, d))
-        Legate.default_alignment(task3, in_vars_4, out_vars_4)
+    # 3. Arbitrary Arg Task (4 args: 2 in, 2 out)
+    task3 = Legate.create_julia_task(rt, lib, my_4arg_task)
+    in_vars_4 = Vector{Legate.Variable}()
+    out_vars_4 = Vector{Legate.Variable}()
+    # Inputs: a, c
+    push!(in_vars_4, Legate.add_input(task3, a))
+    push!(in_vars_4, Legate.add_input(task3, c))
+    # Outputs: b (reuse), d (new)
+    push!(out_vars_4, Legate.add_output(task3, b))
+    push!(out_vars_4, Legate.add_output(task3, d))
+    Legate.default_alignment(task3, in_vars_4, out_vars_4)
 
-        Legate.submit_task(rt, task3)
+    Legate.submit_task(rt, task3)
 
-        # 4. Scalar Arg Task (2 args + scalar)
-        task4 = Legate.create_julia_task(rt, lib, my_scalar_task)
-        in_vars_s = Vector{Legate.Variable}()
-        out_vars_s = Vector{Legate.Variable}()
-        # Input: c (result of task2)
-        push!(in_vars_s, Legate.add_input(task4, c))
-        # Output: a (reuse)
-        push!(out_vars_s, Legate.add_output(task4, a))
+    # 4. Scalar Arg Task (2 args + scalar)
+    task4 = Legate.create_julia_task(rt, lib, my_scalar_task)
+    in_vars_s = Vector{Legate.Variable}()
+    out_vars_s = Vector{Legate.Variable}()
+    # Input: c (result of task2)
+    push!(in_vars_s, Legate.add_input(task4, c))
+    # Output: a (reuse)
+    push!(out_vars_s, Legate.add_output(task4, a))
 
-        # Add user scalar argument (Float32)
-        Legate.add_scalar(task4, Legate.Scalar(2.5f0))
-        Legate.default_alignment(task4, in_vars_s, out_vars_s)
+    # Add user scalar argument (Float32)
+    Legate.add_scalar(task4, Legate.Scalar(2.5f0))
+    Legate.default_alignment(task4, in_vars_s, out_vars_s)
 
-        Legate.submit_task(rt, task4)
+    Legate.submit_task(rt, task4)
 
-        @info "Tasks submitted successfully"
-    end
+    @info "Tasks submitted successfully"
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    async_task = test_driver()
-    wait(async_task)
+    test_driver()
+    # our destructor will call wait_ufi,
+    # but you can manually add a wait here if you want to
+    Legate.wait_ufi()
     @info "All tasks completed."
 end
