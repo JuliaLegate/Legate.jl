f064ffc9a0f19c033048384827553e7223a990a7
krasow <krasow@u.northwestern.edu>
2026-02-03 22:22:57 -0600

    cleanup mode finding

--- src/Legate.jl
+++ src/Legate.jl
@@ -45,34 +45,27 @@ const SUPPORTED_TYPES = Union{
 
 # Sets the LEGATE_LIB_PATH and WRAPPER_LIB_PATH preferences based on mode
 # This will also include the relevant JLLs if necessary.
-@static if LegatePreferences.MODE == "jll"
+MODE = load_preference(LegatePreferences, "legate_mode", LegatePreferences.MODE_JLL)
+@static if MODE == LegatePreferences.MODE_JLL
     using legate_jll, legate_jl_wrapper_jll
     find_paths(
-        LegatePreferences.MODE;
+        MODE;
         legate_jll_module=legate_jll,
         legate_jll_wrapper_module=legate_jl_wrapper_jll,
     )
-elseif LegatePreferences.MODE == "developer"
+elseif MODE == LegatePreferences.MODE_DEVELOPER
     use_legate_jll = load_preference(LegatePreferences, "legate_use_jll", true)
     if use_legate_jll
         using legate_jll
-        find_paths(
-            LegatePreferences.MODE;
-            legate_jll_module=legate_jll,
-            legate_jll_wrapper_module=nothing,
-        )
+        find_paths(MODE; legate_jll_module=legate_jll)
     else
-        find_paths(LegatePreferences.MODE)
+        find_paths(MODE)
     end
-elseif LegatePreferences.MODE == "conda"
-    find_paths(
-        LegatePreferences.MODE,
-        legate_jll_module=nothing,
-        legate_jll_wrapper_module=nothing,
-    )
+elseif MODE == LegatePreferences.MODE_CONDA
+    find_paths(MODE)
 else
     error(
-        "Legate.jl: Unknown mode $(LegatePreferences.MODE)." *
+        "Legate.jl: Unknown mode $(MODE)." *
         "Must be one of 'jll', 'developer', or 'conda'.",
     )
 end
