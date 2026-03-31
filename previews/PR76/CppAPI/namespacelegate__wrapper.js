f8b3dde1ab5c50b7d81d55d959dba8526db7eb33
Documenter.jl <documenter@juliadocs.github.io>
2026-03-31 20:37:26 +0000

    build based on 91b05cc

---
+++ previews/PR76/CppAPI/namespacelegate__wrapper.js
@@ -0,0 +1,23 @@
+var namespacelegate__wrapper =
+[
+    [ "runtime", "namespacelegate__wrapper_1_1runtime.html", [
+      [ "get_runtime", "namespacelegate__wrapper_1_1runtime.html#a5b93eebd06dc138ffd1282dfeb5e95b6", null ],
+      [ "has_finished", "namespacelegate__wrapper_1_1runtime.html#a437ea6ff893ade42f8930d335217c105", null ],
+      [ "has_started", "namespacelegate__wrapper_1_1runtime.html#a1a57d668a29205b6359bf0daa2a669b9", null ],
+      [ "legate_finish", "namespacelegate__wrapper_1_1runtime.html#a12d0a6b01c36260b1a24558ac7170ece", null ],
+      [ "start_legate", "namespacelegate__wrapper_1_1runtime.html#a3736f934f8084f35cf4c4836ce412033", null ]
+    ] ],
+    [ "tasking", "namespacelegate__wrapper_1_1tasking.html", [
+      [ "align", "namespacelegate__wrapper_1_1tasking.html#ab44e8c1225314abbcf6bc94bcadfaace", null ],
+      [ "create_auto_task", "namespacelegate__wrapper_1_1tasking.html#ace28ff19396f4a6a5265e73ce69eea1a", null ],
+      [ "create_manual_task", "namespacelegate__wrapper_1_1tasking.html#acbe51b98d0269a2f59f24ebb3ac20f98", null ],
+      [ "domain_from_shape", "namespacelegate__wrapper_1_1tasking.html#a51cac60f8d12f200427ebad89675323f", null ],
+      [ "submit_auto_task", "namespacelegate__wrapper_1_1tasking.html#a6474300e9646f9bb146b86b72753a6e9", null ],
+      [ "submit_manual_task", "namespacelegate__wrapper_1_1tasking.html#a25062a88fe0a6ea555422c19ca4f2b72", null ]
+    ] ],
+    [ "data", "namespacelegate__wrapper_1_1data.html", "namespacelegate__wrapper_1_1data" ],
+    [ "time", "namespacelegate__wrapper_1_1time.html", [
+      [ "time_microseconds", "namespacelegate__wrapper_1_1time.html#a61077f345c4928adc30d8d7fc7e05671", null ],
+      [ "time_nanoseconds", "namespacelegate__wrapper_1_1time.html#a0197ad196c058b9eeaedd10d6becb985", null ]
+    ] ]
+];
