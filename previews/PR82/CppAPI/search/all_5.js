29105582cb93367919e9e60b87001caf436d9ea0
Documenter.jl <documenter@juliadocs.github.io>
2026-04-23 14:14:55 +0000

    build based on 1c7043f

---
+++ previews/PR82/CppAPI/search/all_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['get_5fptr_0',['get_ptr',['../module_8cpp.html#a78e768f5e7ea789678aa1f834f9d80b1',1,'get_ptr():&#160;module.cpp'],['../namespacelegate__wrapper_1_1data.html#a96a51700e9f9b24eecbab12e7351a522',1,'legate_wrapper::data::get_ptr()']]],
+  ['get_5fruntime_1',['get_runtime',['../namespacelegate__wrapper_1_1runtime.html#a5b93eebd06dc138ffd1282dfeb5e95b6',1,'legate_wrapper::runtime']]],
+  ['getptrfunctor_2',['GetPtrFunctor',['../structlegate__wrapper_1_1data_1_1GetPtrFunctor.html',1,'legate_wrapper::data']]]
+];
