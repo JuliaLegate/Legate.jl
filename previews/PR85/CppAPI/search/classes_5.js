a6ee21c7c7d93de9af79bf15495fea33b9767f76
Documenter.jl <documenter@juliadocs.github.io>
2026-07-31 23:22:56 +0000

    build based on c90066e

---
+++ previews/PR85/CppAPI/search/classes_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ufifunctor_0',['ufiFunctor',['../structufi_1_1ufiFunctor.html',1,'ufi']]]
+];
---
+++ previews/PR85/CppAPI/search/variables_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['scalar_5ftypes_0',['scalar_types',['../structufi_1_1TaskRequestData.html#a6ecb8828ead8376ed4ff12786834c64d',1,'ufi::TaskRequestData']]],
+  ['scalars_5fptr_1',['scalars_ptr',['../structufi_1_1TaskRequestData.html#a3e5194f3da82d04b9426d71a27f5b88f',1,'ufi::TaskRequestData']]]
+];
