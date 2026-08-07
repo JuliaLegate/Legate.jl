d8ec7ef67a27232e68714380478297e742152c9f
Documenter.jl <documenter@juliadocs.github.io>
2026-08-07 14:48:33 +0000

    build based on daa1c8b

---
+++ previews/PR95/CppAPI/search/enums_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['accessmode_0',['AccessMode',['../namespaceufi.html#a1b12f5afea88fad4974b04700aa9122b',1,'ufi']]]
+];
---
+++ previews/PR95/CppAPI/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['module_2ecpp_0',['module.cpp',['../module_8cpp.html',1,'']]]
+];
